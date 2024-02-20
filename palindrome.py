c6fbfa61be269138bb432f11c22aa2aeb49f578f
marchikk <157586063+marchikk@users.noreply.github.com>
2024-02-20 17:51:28 +0000

    addchanges

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+def is_palindrome(word):
+    word = word.lower()
+    
+    
+    return word == word[::-1]
+
+def main():
+    user_input = input("Введіть слово: ")
+
+    if is_palindrome(user_input):
+        print("Це слово - паліндром!")
+    else:
+        print("Це слово не є паліндромом.")
+
+if __name__ == "__main__":
+    main()
